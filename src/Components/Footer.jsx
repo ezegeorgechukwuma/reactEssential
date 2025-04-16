bec13e1395dc7b502e607df319671f8714746f12
george <ezegeorgechukwuma@gmail.com>
2025-04-16 15:03:58 -0400

    my first react app

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <footer>
+            <div class="Footer_container"> {/* not used for now */}
+            <p>&copy; Event Planner Organization. All rights reserved.</p>
+            </div>
+        </footer>
+
+    );
+};
+
+export default Footer;
