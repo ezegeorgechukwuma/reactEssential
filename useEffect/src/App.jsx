f6e592b5425b42e9e199f7390ed22ba5577563b1
george <ezegeorgechukwuma@gmail.com>
2025-04-18 00:39:17 -0400

    fetch data from an api

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import FetchData from './Components/FetchData'
+ import React from 'react'
+ import FetchData from './Components/FetchData'
+ import FetchYogaData from './Components/FetchYogaData'
 
-function App() {
-  return (
- <>
- <FetchData/>
- </>
-  )
-}
+ function App() {
+   return (
+  <>
+  {/* <FetchData/> */}
+   <FetchYogaData/>
+  </>
+   )
+ }
 
-export default App
+ export default App
